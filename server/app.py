abac01b5876ece20fbc96237edeabd4521e0a3dd
Historia Montague <historia.montague@outlook.com>
2023-06-15 11:28:27 -0600

    Finished Lab

--- server/app.py
+++ server/app.py
@@ -17,11 +17,34 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        return make_response(
+            [plant.to_dict() for plant in Plant.query.all()],
+            200
+        )
 
-class PlantByID(Resource):
-    pass
+    def post(self):
+        req = request.get_json()
+        new_plant = Plant(name=req.get("name"),
+                          image=req.get("image"),
+                          price=req.get("price"))
+        db.session.add(new_plant)
+        db.session.commit()
         
+        return make_response(
+            new_plant.to_dict(),
+            201
+        )
+        
+class PlantByID(Resource):
+    def get(self, id):
+        return make_response(
+            Plant.query.filter(Plant.id==id).first().to_dict(),
+            200
+        )
+        
+api.add_resource(Plants, "/plants")
+api.add_resource(PlantByID, "/plants/<int:id>")
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
